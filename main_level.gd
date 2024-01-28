da09c6ae2f27de6af2965ba74368da8d8541f2b6
Troy <troyjsanders@gmail.com>
2024-01-28 15:13:37 -0600

    Added player hand to register area

--- main_level.gd
+++ main_level.gd
@@ -2,4 +2,5 @@ extends Node3D
 
 
 func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
 	pass
--- player.gd
+++ player.gd
@@ -1,11 +1,16 @@
 extends Node3D
 
+var currentPos: Vector2
+var prevPos: Vector2
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+var min_rotation_y = -.05
+var max_rotation_y = .05
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# look_at(screen_point_to_ray(), Vector3.UP)
+	currentPos = get_viewport().get_mouse_position()
+	rotate_y(-(currentPos.x - prevPos.x) * 0.1 * delta)
+
+	prevPos = currentPos
 	pass
